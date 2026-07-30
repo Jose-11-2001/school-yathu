00afe6e6ea2e1b78db20c10090b4ed4c1316c881
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-07-30 08:44:26 +0200

    upade my app

--- obj/Debug/net8.0/school-yathu.AssemblyInfo.cs
+++ obj/Debug/net8.0/school-yathu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("school-yathu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a714bcd2d5bef0ecbf15e34c194e4a736c09ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259c4f062f16f0d1121ad158e7f66a165cf61640")]
 [assembly: System.Reflection.AssemblyProductAttribute("school-yathu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("school-yathu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
